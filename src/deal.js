afd206b48716d7530da6ffad9ebf2897340c47d0
emily baier <emilyrbaier@gmail.com>
2019-02-21 16:55:00 -0800

    finished styling for tutorial and play page, updated global styles

--- src/deal.js
+++ src/deal.js
@@ -1,8 +1,10 @@
 const paragraphOne = document.getElementById('one');
 const paragraphTwo = document.getElementById('two');
 const paragraphThree = document.getElementById('three');
+const paragraphFour = document.getElementById('four');
 const buttonOne = document.getElementById('button-one');
 const buttonTwo = document.getElementById('button-two');
+const buttonThree = document.getElementById('button-three');
 const dealLink = document.getElementById('deal-link');
 const skipTutorial = document.getElementById('skip-tutorial');
 
@@ -16,9 +18,17 @@ function clickTwo(){
     paragraphTwo.classList.add('hidden');
     paragraphThree.classList.remove('hidden');
     buttonTwo.classList.add('hidden');
+    buttonThree.classList.remove('hidden');
+}
+function clickThree(){
+    paragraphThree.classList.add('hidden');
+    paragraphFour.classList.remove('hidden');
+    buttonTwo.classList.add('hidden');
+    buttonThree.classList.add('hidden');
     dealLink.classList.remove('hidden');
     skipTutorial.classList.add('hidden');
 }
 
 buttonOne.addEventListener('click', clickOne);
 buttonTwo.addEventListener('click', clickTwo);
+buttonThree.addEventListener('click', clickThree);
