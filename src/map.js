1b35e7750d639777dc661ed3b051b036480029fe
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-21 16:28:51 -0800

    made party house unclickable without party hat

--- src/map.js
+++ src/map.js
@@ -1,14 +1,26 @@
 import creatureProfile from './profile.js';
 import localLoad from './local-load.js';
-const partyImage = document.getElementById('party-image');
+import localSave from './local-save.js';
+const partyImage = document.getElementById('party');
+const stars = document.getElementById('stars');
 
 creatureProfile();
 
 const profile = localLoad('profile');
 
 if(profile.hat === 3) {
-    partyImage.classList.remove('hidden');
+    partyImage.classList.remove('unclickable-building');
 }
+
+stars.addEventListener('click', function() {
+    if(!profile.starClicked) {
+        profile.happiness += 2;
+        profile.starClicked = true;
+    }
+    localSave('profile', profile);
+    creatureProfile();
+});
+
 
 
 
